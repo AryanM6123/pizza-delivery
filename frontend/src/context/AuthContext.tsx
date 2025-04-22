3290bdd3d1fd407366df5d6150676c5548d76f56
Aryan Mane <149935604+AryanM6123@users.noreply.github.com>
2025-04-22 13:42:01 +0530

    Update AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -14,7 +14,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [user, setUser] = useState(null);
 
   const login = async (email: string, password: string) => {
-    const response = await fetch('http://localhost:5001/api/users/login', {
+    const response = await fetch('https://pizza-delivery-xgd1.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -32,7 +32,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   };
 
   const register = async (name: string, email: string, password: string) => {
-    const response = await fetch('http://localhost:5001/api/users/register', {
+    const response = await fetch('https://pizza-delivery-xgd1.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
